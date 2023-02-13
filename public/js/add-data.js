bf11b12bb1356002cb9fb13ea3e318c3f4bebcf3
JenniferVC <jenniferkt.ifsp@gmail.com>
2023-02-13 00:03:43 -0300

    insert funcionando

--- public/js/add-data.js
+++ public/js/add-data.js
@@ -1,30 +1,102 @@
 var db = firebase.firestore();
 
-function save() {
-    console.log(form.inputBairroNome());
-    // var inputBairroNome = document.getElementById("input-bairro-nome").value;
-    // var inputPontoNome = document.getElementById("input-ponto-nome").value;
-    // var inputParada = document.getElementById("input-parada-id").value;
-    // var inputLatLgn = document.getElementById("input-lat-lgn").value;
-    // var inputHora = document.getElementById("input-hora").value;
-    // var inputTipoLinha = document.getElementById("input-line-type").value;
+var BairrosCollection = db.collection("Bairros");
+var ParadasCollection = db.collection("Paradas");
+var HorarioCollection = db.collection("Horario");
 
-    // db.collection("Paradas").doc(inputPontoNome).set({
-    //     id: inputParada,
-    //     latlgn: inputLatLgn
-    // })
+function selectNeighborhoods() {
+    const bairros = [];
+    const newHTML = '';
+    const inputBairroPai = document.getElementById('input-bairro-nome');
+    const optionBairroFilho = document.createElement('option');
+
+    BairrosCollection.get().then((querySnapshot) => {
+        querySnapshot.forEach((docBairro) => {
+            bairros.push(docBairro.data().nome);
+        });
+    });
+
+    bairros.forEach((nome) => {
+        newHTML += `<option>${nome}</option>`;
+    })
+
+    console.log(newHTML);
+    inputBairroPai.innerHTML = newHTML;
+
+}
+
+function save() {
+    const insert = {
+        nomeBairro: form.inputBairroNome().value,
+        parada: '',
+        horario: '',
+    }
+    const parada = onChangeBusStop();
+    const horario = onChangeTime();
+
+    if (parada && horario) {
+        insert.parada = parada;
+        insert.horario = horario;
+        console.log(insert);
+    }
+    else if (parada) {
+        insert.parada = parada;
+        console.log(insert);
+    }
+    else if (horario) {
+        insert.horario = horario;
+        console.log(insert);
+    }
+
+    BairrosCollection.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data().nome);
+        });
+    });
+
+    // BairrosCollection.add({ "nome": insert.nomeBairro })
     //     .then(function () {
-    //         console.log("Doc successful");
+    //         console.log("Document successfully written!");
     //     })
     //     .catch(function (error) {
-    //         console.error("Error writing doc", error);
+    //         console.error("Error writing document: ", error);
     //     });
+}
+
+function onChangeBusStop() {
+    const parada = {
+        id: parseInt(form.inputParadaID().value),
+        nome: form.inputPontoNome().value,
+        latlgn: form.inputLatLgn().value,
+    }
+
+    if (parada.id && parada.nome && parada.latlgn) {
+        return parada;
+    }
+    else {
+        return false;
+    }
+}
+
+function onChangeTime() {
+    const horario = {
+        tipoLinha: form.inputTipoLinha().value,
+        hora: form.inputHora().value,
+    }
+
+    if (horario.hora && horario.tipoLinha) {
+        return horario;
+    }
+    else {
+        return false;
+    }
 }
 
 const form = {
     inputBairroNome: () => document.getElementById("input-bairro-nome"),
     inputPontoNome: () => document.getElementById("input-ponto-nome"),
-    inputParada: () => document.getElementById("input-parada-id"),
+    inputParadaID: () => document.getElementById("input-parada-id"),
     inputLatLgn: () => document.getElementById("input-lat-lgn"),
     inputHora: () => document.getElementById("input-hora"),
     inputTipoLinha: () => document.getElementById("input-line-type"),
