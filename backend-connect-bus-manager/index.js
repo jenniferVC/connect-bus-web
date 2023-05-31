d2a0a1f956819a9c2b94fcf9318e207aeee7a470
JenniferVC <jenniferkt.ifsp@gmail.com>
2023-05-31 15:34:03 -0300

    Conectar o nodejs com express ao banco firestore
    Foi feito uma chamada GET no endpoint 'bairros' para o firestore

--- backend-connect-bus-manager/index.js
+++ backend-connect-bus-manager/index.js
@@ -1,15 +1,37 @@
 import express from 'express';
+import admin from 'firebase-admin';
 
 const app = express();
 
-// URL: https://connectbus-7d8d9.web.app/pages/bairros/list/list-bairros.html
+/**
+ * Referenciando o arquivo `serviceAccountKey.json` que
+ * possui as credenciais que por sua vez permite que esse back-end faça 
+ * chamadas para o firestore.
+ */
+admin.initializeApp({
+  credential: admin.credential.cert('serviceAccountKey.json'),
+  databaseURL: "https://connectbus-7d8d9-default-rtdb.firebaseio.com"
+});
+
+
 
 //
 // https://expressjs.com/en/guide/routing.html
 //
 app.get('/bairros', (request, response) => {
   console.log('GET bairros');
-  response.json([{id: 1}]);
+  admin.firestore()
+  .collection('Bairros').get().then(snapshot => {
+    const bairros = snapshot.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id
+    }))
+    response.json(bairros);
+  })
+
 })
 
 app.listen(3000, () => console.log('API rest iniciada em http://localhost:3000'))
+
+
+
