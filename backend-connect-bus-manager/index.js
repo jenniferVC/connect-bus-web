e6d0aa019ad1e22e5f7396eced0a04b5ff042e3b
JenniferVC <jenniferkt.ifsp@gmail.com>
2023-05-31 22:10:26 -0300

    Identificar o usuario com JWT
    Foi verificado o JWT enviado pelo Header Authorization da requisição

--- backend-connect-bus-manager/index.js
+++ backend-connect-bus-manager/index.js
@@ -18,16 +18,42 @@ admin.initializeApp({
 //
 // https://expressjs.com/en/guide/routing.html
 //
-app.get('/bairros', (request, response) => {
+app.get('/bairros', async (request, response) => {
+  // Quando o front-end enviar para o backend o JWT, será 
+  // enviado dentro do header authorization
+  const jwt = request.headers.authorization;
+
+  // Caso nao tenha um JWT então é retornado o codigo de erro 
+  // 401 e uma mensagem de nao autorizado
+  if (!jwt) {
+    response.status(401).json({ message: 'Usuário não autorizado' });
+    return;
+  }
+
+  let decodedIdToken = "";
+  try {
+    // Função que verifica se o JWT é valido e 
+    //'true' para verificar se nao expirou.
+    decodedIdToken = await admin.auth().verifyIdToken(jwt, true);
+  } catch (error) {
+    // Se a autenticação com JWT der erro vai cair no catch
+    // e retornar o codigo 401
+    response.status(401).json({ message: 'Usuário não autorizado' });
+    return;
+  }
+
   console.log('GET bairros');
   admin.firestore()
-  .collection('Bairros').get().then(snapshot => {
-    const bairros = snapshot.docs.map(doc => ({
-      ...doc.data(),
-      id: doc.id
-    }))
-    response.json(bairros);
-  })
+    .collection('Bairros')
+    .orderBy("nomeBairro", "asc")
+    .get()
+    .then(snapshot => {
+      const bairros = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }))
+      response.json(bairros);
+    })
 
 })
 
