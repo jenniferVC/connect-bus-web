cd7fa1099c0a4e962ac519ff80e02f1fd7e2fad9
JenniferVC <jenniferkt.ifsp@gmail.com>
2023-06-05 14:52:08 -0300

    Criar testes unitários para o middleware de autenticaçao

--- backend-connect-bus-manager/middlewares/authenticate-jwt.js
+++ backend-connect-bus-manager/middlewares/authenticate-jwt.js
@@ -1,6 +1,5 @@
-import admin from 'firebase-admin';
 
-export async function authenticateToken(request, response, next) {
+export async function authenticateToken(request, response, next, auth) {
   // Quando o front-end enviar para o backend o JWT, será 
   // enviado dentro do header authorization
   const jwt = request.headers.authorization;
@@ -8,7 +7,7 @@ export async function authenticateToken(request, response, next) {
   // Caso nao tenha um JWT então é retornado o codigo de erro 
   // 401 e uma mensagem de nao autorizado
   if (!jwt) {
-    response.status(401).json({ message: '1Usuário não autorizado' });
+    response.status(401).json({ message: 'Não foi fornecido um JWT!' });
     return;
   }
 
@@ -16,17 +15,18 @@ export async function authenticateToken(request, response, next) {
   try {
     // Função que verifica se o JWT é valido e 
     //'true' para verificar se nao expirou.
-    decodedIdToken = await admin.auth().verifyIdToken(jwt, true);
+    decodedIdToken = await auth.verifyIdToken(jwt, true);
   } catch (error) {
     // Se a autenticação com JWT der erro vai cair no catch
     // e retornar o codigo 401
-    response.status(401).json({ message: '2Usuário não autorizado' });
+    response.status(401).json({ message: 'JWT invalido!' });
     return;
   }
 
-  // request.user = {
-  //   uid: decodedIdToken.sub
-  // }
+  // Isso não e usado para nada, apenas para o teste
+  request.user = {
+    uid: decodedIdToken.sub
+  }
 
   next();
 }
--- backend-connect-bus-manager/neighborhoods/routes.js
+++ backend-connect-bus-manager/neighborhoods/routes.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import admin from 'firebase-admin';
 import { authenticateToken } from '../middlewares/authenticate-jwt.js';
 import { NeighborhoodController } from './controller.js';
 
 const app = express();
 
-
 const neighborhoodsController = new NeighborhoodController();
-app.get('/', authenticateToken, neighborhoodsController.listAll);
 
+app.get('/',
+  (request, response, next) => authenticateToken(request, response, next, admin.auth()),
+  (request, response) => neighborhoodsController.listAll(request, response)
+);
 
 export const neighborhoodsRouter = app;
